422817d8dbef4731a6a43b8b68f3ba783f8c4306
kimbohy <lovatina123@gmail.com>
2024-08-29 10:59:49 +0300

    improved Cursus.jsx an Header.jsx

--- src/assets/components/Cursus.jsx
+++ src/assets/components/Cursus.jsx
@@ -2,7 +2,7 @@ import Cart from "./Cart";
 
 function Cursus() {
   return (
-    <div className="bg-slate-900 min-h-screen flex flex-col gap-4 items-center">
+    <div className="bg-slate-900 min-h-screen flex flex-col gap-4 items-center pt-4">
       <Cart title={"test"} description={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse unde ducimus voluptatem. Voluptatibus fugiat aspernatur?"} position={true} />
       <Cart title={"test2"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ratione minima voluptas, iure sed obcaecati , ea neque aspernatur."} position={false} />
       <Cart title={"test3"} description={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eius explicabo perspiciatis, laboriosam voluptatibus."} position={true} />
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,15 +1,15 @@
+import NavBut from "./NavBut";
 import TextChangeOneHover from "./ui/text-change-oneHover";
-
 
 function Header() {
   return (
     <header className="flex justify-between pr-12 pt-5 w-full z-20 pl-5">
       <TextChangeOneHover />
-        <div className="flex justify-between w-max gap-16 text-2xl text-white">
-            <span>Work</span>
-            <span>About</span>
-            <span>Contact</span>
-        </div>
+      <div className="flex justify-between w-max gap-16 text-2xl text-white">
+        <NavBut text={'Work'} D_lay={0.1} />
+        <NavBut text={'About'} D_lay={0.3} />
+        <NavBut text={'Contact'} D_lay={0.2} />
+      </div>
     </header>
   );
 }
