54cc1eced3a7f9051b20874fb115d63f885ffc50
Kimbohy <lovabarwijons@gmail.com>
2025-01-11 20:34:40 +0300

    refactor: update FlipWords component to improve visibility on larger screens

--- src/assets/components/FirstPage/FlipWords.tsx
+++ src/assets/components/FirstPage/FlipWords.tsx
@@ -15,10 +15,10 @@ function FlipWordsCp() {
         <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-second mb-2">
           Welcome
         </h1>
-        <div className="text-base sm:text-lg md:text-xl lg:text-2xl font-normal text-second dark:text-neutral-400">
+        <div className="text-base sm:text-lg md:text-xl lg:text-2xl font-normal text-second dark:text-neutral-400 hidden md:block">
           Emphasize
-          <FlipWords 
-            words={words} 
+          <FlipWords
+            words={words}
             className="ml-2 font-semibold bg-gradient-to-r from-violet-600 to-indigo-600 bg-clip-text text-transparent"
           />
         </div>
