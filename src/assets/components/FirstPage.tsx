fce2bb8040a4f629da1ca0c46ef10c2f9d54a69f
Kimbohy <lovabarwijons@gmail.com>
2025-01-11 21:26:00 +0300

    refactor: comment out unused Boxes component in FirstPage for cleaner code

--- src/assets/components/FirstPage.tsx
+++ src/assets/components/FirstPage.tsx
@@ -1,6 +1,6 @@
 import FlipWordsCp from "./FirstPage/FlipWords";
 import Header from "./FirstPage/Header";
-import { Boxes } from "./ui/background-boxes";
+// import { Boxes } from "./ui/background-boxes";
 import { TextHoverEffect } from "./ui/text-hover-effect";
 
 function FirstPage() {
@@ -14,11 +14,11 @@ function FirstPage() {
       className="relative flex flex-col w-full h-screen overflow-hidden bg-slate-900"
     >
       <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-      <Boxes />
+      {/* <Boxes /> */}
       <Header />
       <div className="flex flex-col h-full justify-center relative px-4 sm:px-0">
         <p
-          className="absolute top-1/4 sm:top-1/3 md:top-1/3 lg:top-2/3
+          className="absolute top-1/4 sm:top-1/3 md:top-1/3 lg:top-2/4
           right-4 sm:right-8 md:right-36 lg:right-72 
           text-second text-2xl sm:text-3xl md:text-5xl lg:text-[6em] 
           z-[21] pointer-events-none 
