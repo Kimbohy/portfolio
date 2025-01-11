7d963c1302dfe469da4b977319cb58d4de7c9991
Kimbohy <lovabarwijons@gmail.com>
2025-01-11 22:17:49 +0300

    refactor: update TerminalPart component to improve responsiveness and visibility

--- src/assets/components/TerminalPart.tsx
+++ src/assets/components/TerminalPart.tsx
@@ -2,7 +2,10 @@ import Window from "./Terminal/Window";
 
 const TerminalPart = () => {
   return (
-    <div id="terminal" className="bg-slate-900 flex h-[90vh] p-4 md:p-12 lg:p-24">
+    <div
+      id="terminal"
+      className="bg-slate-900 md:flex h-[90vh] p-4 md:p-12 lg:p-24 hidden"
+    >
       <Window />
     </div>
   );
