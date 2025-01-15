e4bb7f5a5e38847061c9c3b295d38b66de15d59c
Kimbohy <lovabarwijons@gmail.com>
2025-01-15 10:02:35 +0300

    refactor: implement dynamic frame switching in Glitch component for enhanced visual effects

--- src/assets/components/FirstPage/glitchSvg/Glitch.tsx
+++ src/assets/components/FirstPage/glitchSvg/Glitch.tsx
@@ -1,10 +1,48 @@
-import logo from "/kim.svg";
+import { useState, useEffect } from "react";
+
+const path = "/images/logo/";
+const framesName = [
+  "Frame 1.svg",
+  "Frame 2.svg",
+  "Frame 3.svg",
+  "Frame 4.svg",
+  "Frame 5.svg",
+];
 
 export default function Glitch() {
+  const [currentFrame, setCurrentFrame] = useState(0);
+
+  useEffect(() => {
+    const changeFrame = () => {
+      let randomFrame;
+      do {
+        randomFrame = Math.floor(Math.random() * framesName.length);
+      } while (randomFrame === currentFrame);
+
+      setCurrentFrame(randomFrame);
+    };
+
+    const randomInterval = () => {
+      if (currentFrame === 0) {
+        return Math.random() * (5000 - 3000) + 3000; // Stay longer on frame 1
+      } else {
+        return Math.random() * (500 - 50) + 50; // Faster on other frames
+      }
+    };
+
+    const interval = setInterval(changeFrame, randomInterval());
+
+    return () => clearInterval(interval);
+  }, [currentFrame]);
+
   return (
     <div className="flex items-center">
-      <div className="relative w-[80px]">
-        <img src={logo} alt="logo" className="w-full h-full" />
+      <div className="relative w-[100px]">
+        <img
+          src={`${path}${framesName[currentFrame]}`}
+          alt="logo"
+          className="w-full h-full"
+        />
       </div>
     </div>
   );
